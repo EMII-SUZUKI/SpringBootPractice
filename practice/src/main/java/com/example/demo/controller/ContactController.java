027529850a0f856a67bf3b87318e5e938f210587
[EMII-SUZUKI] <[e.suzuki.cingroup2012@gmail.com]>
2024-05-16 15:45:26 +0900

    お問い合わせフォーム実装 その2

--- practice/src/main/java/com/example/demo/controller/ContactController.java
+++ practice/src/main/java/com/example/demo/controller/ContactController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.data.ContactData;
--- practice/src/main/java/com/example/demo/data/ContactData.java
+++ practice/src/main/java/com/example/demo/data/ContactData.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class ContactData {
 	private String lastName;
-    private String firstName;
-    private String email;
-    private String phone;
-    private String zipCode;
-    private String address;
-    private String buildingName;
-    private String contactType;
-    private String body;
+	private String firstName;
+	private String email;
+	private String phone;
+	private String zipCode;
+	private String address;
+	private String buildingName;
+	private String contactType;
+	private String body;
 }
